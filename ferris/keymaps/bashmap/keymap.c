352d2820e1a681dc690425303b6780dee4c318fb
bashNeko <bashnko@users.noreply.github.com>
2026-04-10 22:24:13 +0530

    feat(bashmap): add shift-tab macro and layer comments

--- ferris/keymaps/bashmap/keymap.c
+++ ferris/keymaps/bashmap/keymap.c
@@ -10,7 +10,8 @@ void matrix_init_user(void) {
 enum custom_keycodes {
     CTRL_SPC = SAFE_RANGE,
     CTRL_BSPC,
-    CTRL_Y
+    CTRL_Y,
+    SFT_TAB
 };
 
 // Macros 
@@ -37,6 +38,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_code(KC_LCTL);
             }
             return false;
+        case SFT_TAB:
+            if (record->event.pressed){
+                register_code(KC_LSFT);
+                tap_code(KC_TAB);
+                unregister_code(KC_LSFT);
+            }
+            return false;
     }
     return true;
 }
@@ -61,24 +69,31 @@ combo_t key_combos[]= {
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    //base
 	[0] = LAYOUT(
         KC_B, KC_L, KC_D, KC_W, KC_Z, KC_QUOT, KC_F, KC_O, KC_U, KC_J,
         KC_N, KC_R, KC_T, KC_S, KC_G, KC_Y, KC_H, KC_A, KC_E, KC_I,
         KC_Q, LT(3,KC_X), KC_M, KC_C, KC_V, KC_K, KC_P, KC_COMM, KC_DOT, KC_SLSH,
                   KC_LSFT, LT(1, KC_ENT), LT(2, KC_SPC), RCTL_T(KC_ESC)
     ),
+
+    //symbol
 	[1] = LAYOUT(
         KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_NO, KC_LCBR, KC_RCBR, KC_MINS, KC_NO,
         KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC, KC_TAB, KC_EQL, KC_LPRN, KC_RPRN, KC_COLN, KC_DQT,
         KC_BSLS, KC_CIRC, KC_UNDS, KC_TILD, KC_NO, KC_GRV, KC_PIPE, KC_PLUS, KC_NO, KC_SCLN, 
                           KC_1, KC_2, KC_3, KC_4
     ),
+
+    //num
 	[2] = LAYOUT(
-        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_NO, KC_1, KC_2, KC_3, KC_NO,
+        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, SFT_TAB, KC_1, KC_2, KC_3, KC_NO,
         CTRL_SPC, KC_ASTR, KC_LBRC, KC_RBRC, KC_TAB, KC_EQL, KC_4, KC_5, KC_6, KC_0,
         KC_BSLS, KC_CIRC, KC_UNDS, KC_TILD, KC_NO, KC_NO, KC_7, KC_8, KC_9, KC_NO,
                           KC_LGUI, CTRL_BSPC, KC_3, KC_4
     ),
+
+    //nav
 	[3] = LAYOUT(
         KC_Q, KC_W, KC_E, KC_R, QK_BOOT, KC_Y, KC_PGUP, KC_PGDN, KC_O,   KC_P,
         KC_A, KC_S, KC_LCTL, KC_F, KC_G, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_SCLN,
